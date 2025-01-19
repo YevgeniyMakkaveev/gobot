333f966a4bf5b36cbcdc079e6429a64b5d5e49ca
YM <visons29@gmail.com>
2025-01-19 19:52:43 +0300

    mid progress

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello")
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic(err)
+	}
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+
+			switch update.Message.Command() {
+			case "help":
+				helpComand(bot, update.Message)
+			default:
+				defaultMsg(bot, update.Message)
+			}
+
+		}
+	}
+}
+
+func helpComand(bot *tgbotapi.BotAPI, inputMsg *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, "Help is on the way")
+	msg.ReplyToMessageID = inputMsg.MessageID
+
+	bot.Send(msg)
+}
+func listCommand(bot *tgbotapi.BotAPI, inputMsg *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, "TBD")
+	msg.ReplyToMessageID = inputMsg.MessageID
+
+	bot.Send(msg)
+}
+func defaultMsg(bot *tgbotapi.BotAPI, inputMsg *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, "You wrote "+inputMsg.Text)
+	msg.ReplyToMessageID = inputMsg.MessageID
+
+	bot.Send(msg)
 }
